54b8f09b6a5dc7c44f715be06008d2a3e002009b
Connor Cowan <ccowan003@comcast.net>
2019-08-14 17:05:42 -0700

    TEST

--- lib/students.rb
+++ lib/students.rb
@@ -1,9 +1,10 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
 end
 
 def lowest_student_gpa
+  #test
 end
 
 def average_student_gpa
