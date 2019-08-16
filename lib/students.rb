12287e245195a50bf2aa1c055aacfd3596304d61
Connor Cowan <ccowan003@comcast.net>
2019-08-15 17:30:02 -0700

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) AS total_tardies FROM students;"
+  "SELECT SUM(tardies) AS total_tardies FROM students;"   #'AS total_tardies == aliasing the return value'
 end
 
 def average_gpa_for_9th_grade
